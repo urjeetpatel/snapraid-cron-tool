a6ed1f26eac30e102d7bcbbb9f1f3e4779da26e7
Urjeet Patel <urjeet@gmail.com>
2021-10-29 11:53:39 -0500

    feat: :tada: add black and basic initial script

---
+++ snapraid_cron_tool/__main__.py
@@ -0,0 +1,10 @@
+import argparse
+from snapraid_cron_tool.utils.config import Config
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
--- tests/test_snapraid_cron_tool.py
+++ tests/test_snapraid_cron_tool.py
@@ -2,4 +2,4 @@ from snapraid_cron_tool import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.0.1"
